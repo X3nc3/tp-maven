ffe3f893d6f8f2913a9b1c5469c2c779cfdea9ba
Maxence <maxence.ogier@gmail.com>
2024-10-16 17:25:30 +0200

    tp-1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,8 +2,7 @@ package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
 
-public class App 
-{
+public class App {
     public static void main(String[] args) {
 
         String asciiArt = FigletFont.convertOneLine("APP");
